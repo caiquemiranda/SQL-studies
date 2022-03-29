861471ab3fa8deb0386d9b1722a3d266667c9508
caiquemiranda <caiquemirandas@gmail.com>
2022-03-29 05:21:17 -0300

    update

--- insert.sql
+++ insert.sql
@@ -2,15 +2,15 @@
 USE base_de_dados;
 
 -- Mostra as tabelas da base de dadose
-show tables;
+show tabelas;
 
 -- Descreve as colunas da tabela
-describe users;
+describe usuarios;
 
 -- Inserir registros na base de dados
-INSERT INTO users 
+INSERT INTO usuarios 
     (first_name, last_name, email, password_hash) 
     VALUES
-        ("Helena", "A.", "1@email.com", "3_hash"),
-        ("Joana", "B.", "2@email.com", "4_hash"),
-        ("Rosana", "C.", "3@email.com", "5_hash");
+        ("Helena", "A.", "1@email.com", "3_hash")
+        ,("Joana", "B.", "2@email.com", "4_hash")
+        ,("Rosana", "C.", "3@email.com", "5_hash");
